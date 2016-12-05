6d7794b6de5581866bf3bc8ee76ccf0727558593
Gus The IT Guru <gus@gustheitguru.com>
2016-12-05 09:21:49 -0700

    ports and public folders
    adding ports and public folders.

--- server.js
+++ server.js
@@ -2,16 +2,27 @@
 var express = require('express');
 // will turn express into a funcition 
 var app = express();
+var port = 3000;
 
 //takes 2 arguments (root doc, function (request , response) )
-app.get('/', function (req, res) {
-	res.send('hello express!');
-});
+//by adding the folder we do not need to point main root to another area
+// app.get('/', function (req, res) {
+// 	res.send('hello express!');
+// });
 
 app.get('/about', function (req, res) {
 	res.send('About US');
 });
 
+//print directory path in node
+// console.log(__dirname);
+
+//expose folder for web server 
+app.use(express.static(__dirname + '/public'));
+
 //have to tell it what port to listen on
-app.listen(3000);
+//ading second function for to print out to console
+app.listen(port, function (){
+	console.log('express server started on port ' + port);
+});
 
