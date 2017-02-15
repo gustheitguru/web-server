901e1b0913edc91cbb9e94a24ff8addde62ffa44
Gus Rodriguez <Gus@csha.net>
2017-02-15 09:26:33 -0700

    Refactor Middleware

---
+++ middleware.js
@@ -0,0 +1,17 @@
+
+
+
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+//this will expose code for the rest of the app can use this file. 
+module.exports = middleware;
